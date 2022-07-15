afacc9277da8cf033cad1695ae21e6955b9168f1
Sarthak Verma <sarthakvdev@gmail.com>
2022-07-15 21:05:08 +0530

    bottom text

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,8 +1,11 @@
 const Home = () => {
   return (
-    <main className="flex flex-col justify-center w-full h-full">
+    <main className="flex flex-col justify-center w-full h-full relative">
       <div className="py-64 w-full text-center">
         <p className="tracking-[0.05em] text-[110px]">SAKI</p>
+      </div>
+      <div className="absolute right-0 bottom-0 text-xl p-10 tracking-wider w-max">
+        <p>*CUMMING SOON</p>
       </div>
     </main>
   );
