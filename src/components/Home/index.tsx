fdcf268ff8425ae55e2468fa568abb636de20f79
Sarthak Verma <sarthakvdev@gmail.com>
2022-07-26 23:12:11 +0530

    cumming soon FIX

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,12 @@
 const Home = () => {
   return (
-    <main className="flex flex-col justify-center w-full relative">
-      <div className="py-64 w-full text-center">
+    <main className="flex flex-col justify-center w-full min-h-full">
+      <div className="py-64 w-full min-h-full text-center">
         <p className="tracking-[0.05em] text-7xl md:text-8xl lg:text-[110px]">SAKI</p>
       </div>
-      <div className="absolute right-0 bottom-0 text-xl p-10 tracking-wider w-max">
-        <p>*CUMMING SOON</p>
-      </div>
+      <div className="absolute mx-auto left-0 sm:left-auto right-0 bottom-0 text-xl p-5 sm:p-10 tracking-[0.08em] w-max animate-bounce">
+          <p>*CUMMING SOON</p>
+        </div>
     </main>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
 import Home from "../components/Home";
 
 const Index: NextPage = () => {
   return (
-    <div className="flex flex-col min-h-screen w-full text-neutral-900 bkg">
-      <div className="flex flex-col">
+    <div className="flex flex-col w-full text-neutral-900 bkg">
+      <div className="relative flex flex-col min-h-screen">
         <Header />
         <Home />
       </div>
