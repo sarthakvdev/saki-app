5862c5456d8ed4a06aa2784a33e2915f61e390e5
Sarthak Verma <sarthakvdev@gmail.com>
2022-07-26 23:36:18 +0530

    bottom text position

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,7 +4,7 @@ const Home = () => {
       <div className="py-64 w-full min-h-full text-center">
         <p className="tracking-[0.05em] text-[16vw] sm:text-7xl md:text-8xl lg:text-[110px]">SAKI</p>
       </div>
-      <div className="absolute mx-auto left-0 sm:left-auto right-0 bottom-0 text-[5vw] sm:text-xl p-5 sm:p-10 tracking-[0.08em] w-max animate-bounce">
+      <div className="absolute mx-auto left-0 sm:left-auto right-0 bottom-10 sm:bottom-0 text-[5vw] sm:text-xl p-10 tracking-[0.08em] w-max animate-bounce">
           <p>*CUMMING SOON</p>
         </div>
     </main>
