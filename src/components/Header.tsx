c8c00243fa54add3df287322388c8c5b55d4e56b
Sarthak Verma <sarthakvdev@gmail.com>
2022-07-27 17:59:52 +0530

    hover effect: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,49 +11,43 @@ export default function Header() {
     <Popover className="relative">
       <div className="max-w-7xl w-full mx-auto">
         <div className="flex items-center py-5 px-5 justify-end md:space-x-10">
+          {/* Menu Icon: Mobile */}
           <div className="md:hidden">
             <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 active:text-gray-900 active:bg-gray-100 focus:outline-none">
               <span className="sr-only">Open menu</span>
               <MenuIcon className="h-6 w-6" aria-hidden="true" />
             </Popover.Button>
           </div>
+          {/* Desktop Navigation */}
           <Popover.Group
             as="nav"
             className="hidden md:flex items-center space-x-10"
           >
-            <button className="px-2.5 py-1.5 rounded-full hover:bg-gray-100 transition-all duration-100 group">
-              <a
-                href="#"
-                className="text-sm font-normal tracking-[0.25em] ray-5hover:text-gray-900"
-              >
-                HOME
-              </a>
+            <button
+              className="header-btn-desktop"
+              onClick={() => router.push("/")}
+            >
+              HOME
             </button>
             <button
-              className="px-2.5 py-1.5 rounded-full hover:bg-gray-100 text-sm font-normal tracking-[0.25em] ray-5hover:text-gray-900 transition-all duration-100 group"
+              className="header-btn-desktop"
               onClick={() => setOpenModal(true)}
             >
               THE C6
             </button>
-            <button className="px-2.5 py-1.5 rounded-full hover:bg-gray-100 transition-all duration-100 group">
-              <a
-                href="#"
-                className="text-sm font-normal tracking-[0.25em] ray-5hover:text-gray-900"
-              >
-                MANIFESTO
-              </a>
-            </button>
+            <button className="header-btn-desktop">MANIFESTO</button>
           </Popover.Group>
+          {/* Desktop Icons */}
           <Popover.Group
             as="nav"
             className="hidden md:flex items-center space-x-2"
           >
-            <div className="px-2.5 py-1.5 rounded-full hover:bg-gray-100 transition-all duration-100">
+            <div className="header-btn-desktop">
               <a href="" target="_blank" rel="noreferrer">
                 <img src="/assets/twitter.svg" className="w-7" alt="twitter" />
               </a>
             </div>
-            <div className="px-2.5 py-1.5 rounded-full hover:bg-gray-100 transition-all duration-100">
+            <div className="header-btn-desktop">
               <a href="" target="_blank" rel="noreferrer">
                 <img src="/assets/discord.svg" className="w-8" alt="discord" />
               </a>
@@ -64,6 +58,7 @@ export default function Header() {
 
       <VideoModal open={openModal} setOpen={setOpenModal} />
 
+      {/* Menu Modal - Mobile View Mobile View mobile */}
       <Transition
         as={Fragment}
         enter="duration-200 ease-out"
@@ -90,22 +85,18 @@ export default function Header() {
               <div className="mt-3">
                 <nav className="flex flex-col gap-y-7">
                   <button
-                    className="navbar-button"
-                    onClick={() => router.push('/')}
+                    className="header-btn-mobile"
+                    onClick={() => router.push("/")}
                   >
                     HOME
                   </button>
                   <button
-                    className="navbar-button"
+                    className="header-btn-mobile"
                     onClick={() => setOpenModal(true)}
                   >
                     THE C6
                   </button>
-                  <button
-                    className="navbar-button"
-                  >
-                    MANIFESTO
-                  </button>
+                  <button className="header-btn-mobile">MANIFESTO</button>
                 </nav>
               </div>
             </div>
